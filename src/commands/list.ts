72cb08d2bda3c89cba2b003ebd982eb29b074edd
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-24 18:04:30 -0300

    refactor: change way to instanciate task service

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,8 +1,9 @@
 import { Command } from 'commander';
 
+import { TaskRepository } from '@repositories/task.repository';
 import { TaskService } from '@services/task.service';
 
-const taskService = new TaskService();
+const taskService = new TaskService(new TaskRepository());
 
 export const list = new Command('list')
   // TODO: add functionality to list by status provided by user input
