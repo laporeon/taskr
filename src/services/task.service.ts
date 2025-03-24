d992718c6d32e66adaccd7fd9d9c903c63e21a48
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-24 18:04:36 -0300

    refactor: change way to instanciate task repository

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -2,9 +2,7 @@ import { TaskInput } from '@interfaces/Task';
 import { TaskRepository } from '@repositories/task.repository';
 
 export class TaskService {
-  constructor(
-    private readonly taskRepository: TaskRepository = new TaskRepository(),
-  ) {}
+  constructor(private readonly taskRepository: TaskRepository) {}
 
   async create({ title, description }: TaskInput) {
     await this.taskRepository.add({ title, description });
