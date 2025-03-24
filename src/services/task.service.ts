aa3f96ac3d93a155c650f41fa9e1aafc4fd13b64
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-24 16:38:01 -0300

    feat: create add and list services

---
+++ src/services/task.service.ts
@@ -0,0 +1,17 @@
+import { TaskInput } from '@interfaces/Task';
+import { TaskRepository } from '@repositories/task.repository';
+
+export class TaskService {
+  constructor(
+    private readonly taskRepository: TaskRepository = new TaskRepository(),
+  ) {}
+
+  async create({ title, description }: TaskInput) {
+    await this.taskRepository.add({ title, description });
+  }
+
+  async list() {
+    const tasks = await this.taskRepository.get();
+    console.log({ tasks });
+  }
+}
