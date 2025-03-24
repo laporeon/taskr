6171e62c5009c7da97106c6725e41bdfac4cbfb4
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-24 16:39:15 -0300

    feat: instanciate commander and add first commands

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello, World!');
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+import { add, list } from '@commands/index';
+import { logo } from '@utils/logo';
+
+logo();
+
+program.addCommand(add);
+program.addCommand(list);
+
+program
+  .name('pomotimer')
+  .description('A Pomodoro CLI timer.')
+  .version('1.0.0')
+  .addHelpText(
+    'after',
+    `\nExamples:
+  $ taskr -a "Clean node_modules"
+  $ taskr -a "Study" "Do 5 leetcode exercises"`,
+  )
+  .showSuggestionAfterError();
+
+program.parse();
