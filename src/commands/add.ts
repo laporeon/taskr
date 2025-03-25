3b089d379b2efc0a1883627481cadb9581b5901a
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-25 15:07:56 -0300

    feat: add task priority as optional argument and validate user input

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,14 +1,27 @@
 import { Command } from 'commander';
 
+import { TaskPriority, Colors } from '@enums/index';
 import { TaskRepository } from '@repositories/task.repository';
 import { TaskService } from '@services/task.service';
 
 const taskService = new TaskService(new TaskRepository());
 
+const priorityAllowedValues = Object.values(TaskPriority);
+
 export const add = new Command('add')
   .argument('<title>', 'Title of the task')
-  .argument('[description]', 'Optional task description')
-  .action(async (title: string, description?: string) => {
-    await taskService.create({ title, description });
+  .argument(
+    '[priority]',
+    `Optional task priority. Values must be: high, medium or low.`,
+    TaskPriority.LOW,
+  )
+  .action(async (title: string, priority: TaskPriority) => {
+    if (!priorityAllowedValues.includes(priority as TaskPriority)) {
+      return console.error(
+        `${Colors.red}Error: Invalid value for priority: "${priority}". Allowed values are: ${priorityAllowedValues.join(', ')}.${Colors.reset}`,
+      );
+    }
+
+    await taskService.create({ title, priority });
   })
-  .description('Add a new task with an optional description.');
+  .description('Add a new task.');
