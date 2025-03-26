81d8d332ad9fdd19b8ccfd7ad61a89992ed5d7f3
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-26 16:39:59 -0300

    refactor: change status attribute to option instead or optional argument

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,13 +1,27 @@
 import { Command } from 'commander';
 
+import { Colors } from '@enums/index';
+import { TaskStatus } from '@enums/TaskStatus';
 import { TaskRepository } from '@repositories/task.repository';
 import { TaskService } from '@services/task.service';
 
 const taskService = new TaskService(new TaskRepository());
 
+const statusAllowedValues = Object.values(TaskStatus);
+
 export const list = new Command('list')
-  .argument('[status]', 'Optional task status.')
-  .action(async (status: string) => {
+  .option(
+    '-s --status <status>',
+    'Optional new task status. Value must be: todo, in-progress or done.',
+  )
+  .action(async options => {
+    const { status } = options;
+
+    if (status && !statusAllowedValues.includes(status as TaskStatus)) {
+      return console.error(
+        `${Colors.red}Error: Invalid value for priority: "${status}". Allowed values are: todo, in-progress or done.`,
+      );
+    }
     await taskService.list(status);
   })
   .description('List all tasks or list tasks by status.');
