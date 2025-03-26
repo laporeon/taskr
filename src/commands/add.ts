f25bb90ffa73645d6433c124bc69c09d75db54db
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-26 16:39:37 -0300

    refactor: change priority attribute to option instead or optional argument

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -10,15 +10,17 @@ const priorityAllowedValues = Object.values(TaskPriority);
 
 export const add = new Command('add')
   .argument('<title>', 'Title of the task')
-  .argument(
-    '[priority]',
-    `Optional task priority. Values must be: high, medium or low.`,
+  .option(
+    '-p --priority <priority>',
+    'Optional task priority. Value must be: high, medium or low.',
     TaskPriority.LOW,
   )
-  .action(async (title: string, priority: TaskPriority) => {
+  .action(async (title: string, options) => {
+    const { priority } = options;
+
     if (!priorityAllowedValues.includes(priority as TaskPriority)) {
       return console.error(
-        `${Colors.red}Error: Invalid value for priority: "${priority}". Allowed values are: ${priorityAllowedValues.join(', ')}.${Colors.reset}`,
+        `${Colors.red}Error: Invalid value for priority: "${priority}". Allowed values are: high, medium or low.`,
       );
     }
 
