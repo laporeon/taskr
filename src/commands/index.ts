7078f4a2a0cf6c2ae58073d940c2e38ba12791b7
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-26 14:50:10 -0300

    feat: add remove task command to array of available commands

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,6 +1,7 @@
 import { Command } from 'commander';
 
 import { add } from './add';
+import { remove } from './delete';
 import { list } from './list';
 
-export const commands: Command[] = [add, list];
+export const commands: Command[] = [add, list, remove];
