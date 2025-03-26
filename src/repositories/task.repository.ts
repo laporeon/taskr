7e1eac84c735da0ab13afb0dab0229a388b59374
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-26 16:42:19 -0300

    feat: start implementation of update method and remove reset at the end of console log from delete method

--- src/repositories/task.repository.ts
+++ src/repositories/task.repository.ts
@@ -2,7 +2,7 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 
 import { TaskStatus, Colors } from '@enums/index';
-import { Task, TaskInput } from '@interfaces/Task';
+import { CreateTaskInput, Task } from '@interfaces/Task';
 
 export class TaskRepository {
   private readonly filePath = path.resolve(
@@ -11,13 +11,13 @@ export class TaskRepository {
   );
 
   async generateId() {
-    const tasks = await this.get('');
+    const tasks = await this.get();
 
     return tasks.length + 1;
   }
 
-  async add({ title, priority }: TaskInput) {
-    const tasks = await this.get('');
+  async add({ title, priority }: CreateTaskInput) {
+    const tasks = await this.get();
 
     const id = await this.generateId();
 
@@ -32,12 +32,10 @@ export class TaskRepository {
     tasks.push(task);
 
     await fs.writeFile(this.filePath, JSON.stringify(tasks, null, 2));
-    console.log(
-      `\n${Colors.green}✔ ${Colors.gray}Task successfully created!${Colors.reset}`,
-    );
+    console.log(`\n${Colors.green}✔ ${Colors.gray}Task successfully created!`);
   }
 
-  async get(status: string) {
+  async get(status?: TaskStatus | undefined) {
     const fileContent = await fs.readFile(this.filePath, 'utf-8');
     const tasks: Task[] = JSON.parse(fileContent);
 
@@ -48,16 +46,18 @@ export class TaskRepository {
     return tasks.filter(task => task.status === status);
   }
 
+  // TODO: finish implementation of update method
+  async update({ id, title, status, priority }: Partial<Task>) {
+    console.log({ id, title, status, priority });
+  }
+
   // TODO: refactor delet method
   async delete(id: number) {
-    let tasks = await this.get('');
+    let tasks = await this.get();
 
     const task = tasks.find(task => task.id === id);
 
-    if (!task)
-      return console.error(
-        `${Colors.red}Error: Task not found.${Colors.reset}`,
-      );
+    if (!task) return console.error(`${Colors.red}Error: Task not found.`);
 
     tasks = tasks.filter(task => task.id != id);
     await fs.writeFile(this.filePath, JSON.stringify(tasks, null, 2));
