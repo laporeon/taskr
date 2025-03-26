f8b3733952cf4f5ff4fa791e523cb3aadd801a4a
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-26 14:50:54 -0300

    refactor: remove unused colors

--- src/enums/Colors.ts
+++ src/enums/Colors.ts
@@ -1,29 +1,13 @@
 export enum Colors {
-  bold = '\x1b[1m',
-  dim = '\x1b[2m',
-  italic = '\x1b[3m',
-  underline = '\x1b[4m',
-  inverse = '\x1b[7m',
+  bold_underline = '\x1b[1;4m',
   strikethrough = '\x1b[9m',
 
-  black = '\x1b[30m',
   red = '\x1b[31m',
   green = '\x1b[32m',
   yellow = '\x1b[33m',
   blue = '\x1b[34m',
-  magenta = '\x1b[35m',
-  cyan = '\x1b[36m',
-  white = '\x1b[37m',
+  orange = '\x1b[38;5;166m',
   gray = '\x1b[90m',
-
-  bgBlack = '\x1b[40m',
-  bgRed = '\x1b[41m',
-  bgGreen = '\x1b[42m',
-  bgYellow = '\x1b[43m',
-  bgBlue = '\x1b[44m',
-  bgMagenta = '\x1b[45m',
-  bgCyan = '\x1b[46m',
-  bgWhite = '\x1b[47m',
 
   reset = '\x1b[0m',
 }
