2302cfba20dc7c1fa7ec54a3b930e99e98cf6f36
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-26 20:05:54 -0300

    refactor: create a single validate function

--- src/utils/Validator.ts
+++ src/utils/Validator.ts
@@ -2,26 +2,28 @@ import { Colors, TaskPriority, TaskStatus } from '@enums/index';
 
 type IsValidEnum = TaskPriority | TaskStatus;
 
+type ValidationField = {
+  value: string;
+  enum: Record<string, IsValidEnum>;
+  fieldName: string;
+};
+
 export class Validator {
   private errors: string[] = [];
 
-  validateField(
-    value: string,
-    enumType: Record<string, IsValidEnum>,
-    fieldName: string,
-  ) {
-    const validValues = Object.values(enumType) as string[];
+  validate(fields: ValidationField[]) {
+    fields.forEach(({ value, enum: enumType, fieldName }) => {
+      const validValues = Object.values(enumType) as string[];
+      const formattedValidValues = validValues
+        .join(', ')
+        .replace(/, ([^,]+)$/, ' or $1'); // output: x, y or z.
 
-    if (value && !validValues.includes(value)) {
-      this.errors.push(
-        `Invalid ${fieldName} value: "${value}". Valid values are: ${validValues.join(', ')}.`,
-      );
-    }
-  }
-
-  validate(status: TaskStatus, priority: TaskPriority) {
-    this.validateField(status, TaskStatus, 'status');
-    this.validateField(priority, TaskPriority, 'priority');
+      if (value && !validValues.includes(value)) {
+        this.errors.push(
+          `Invalid value for ${fieldName}: "${value}". Value must be: ${formattedValidValues}.`,
+        );
+      }
+    });
 
     if (this.errors.length > 0) {
       console.log(Colors.red + this.errors.join('\n'));
