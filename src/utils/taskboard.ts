6b4073b337ab25e5609cd9a37d04bcff2bd72ef2
laporeon <175569928+daspeon@users.noreply.github.com>
2025-03-26 14:52:23 -0300

    feat: create method to render taskboard list

---
+++ src/utils/taskboard.ts
@@ -0,0 +1,33 @@
+import { Colors, TaskPrioritySymbols, TaskStatusSymbols } from '@enums/index';
+import { Task } from '@interfaces/Task';
+
+const renderTaskboard = (tasks: Task[]) => {
+  const toDoTasksCount = tasks.filter(task => task.status === 'todo').length;
+  const inProgressTasksCount = tasks.filter(
+    task => task.status === 'in-progress',
+  ).length;
+  const finishedTasksCount = tasks.filter(
+    task => task.status === 'done',
+  ).length;
+
+  console.log(`\n  ${Colors.bold_underline}Tasks${Colors.reset}`);
+  tasks.map(({ id, title, status, priority }: Task) => {
+    const statusSymbol = TaskStatusSymbols[status];
+    const prioritySymbol = TaskPrioritySymbols[priority];
+
+    console.log(
+      `   ${Colors.gray}${id}. ${statusSymbol}${title}${Colors.reset} ${prioritySymbol}`,
+    );
+  });
+
+  // TODO: refactor this log
+  console.log(
+    [
+      `\n${Colors.orange}${toDoTasksCount} ${Colors.reset}todo`,
+      `${Colors.blue}${inProgressTasksCount} ${Colors.reset}in-progress`,
+      `${Colors.green}${finishedTasksCount} ${Colors.reset}done\n`,
+    ].join(' · '),
+  );
+};
+
+export { renderTaskboard };
