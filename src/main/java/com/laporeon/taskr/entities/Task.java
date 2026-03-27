6af90408a28650e3cfc30a946ffa6415c7bad5c7
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-27 16:34:55 -0300

    feat(cli): create Task entity setting up defalt values and a custom toString method

---
+++ src/main/java/com/laporeon/taskr/entities/Task.java
@@ -0,0 +1,37 @@
+package com.laporeon.taskr.entities;
+
+import com.laporeon.taskr.enums.TaskPriority;
+import com.laporeon.taskr.enums.TaskStatus;
+import lombok.Builder;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Builder
+public class Task {
+
+    @Builder.Default
+    private UUID id = UUID.randomUUID();
+
+    private String title;
+
+    private String description;
+
+    @Builder.Default
+    private TaskStatus status = TaskStatus.TODO;
+
+    @Builder.Default
+    private TaskPriority priority = TaskPriority.LOW;
+
+    @Builder.Default
+    private Instant createdAt = Instant.now();
+
+    private Instant updatedAt;
+
+    public String toString() {
+        return """
+               %s. %s %s %s
+               """.formatted(id, title, status.getSymbol(), priority.getSymbol());
+    }
+
+}
