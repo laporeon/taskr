fde9d910847dec84c70120a3418531c41cb9cefe
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-30 15:28:04 -0300

    chore(commands): add status option with default value and remove description option

--- src/main/java/com/laporeon/taskr/commands/AddCommand.java
+++ src/main/java/com/laporeon/taskr/commands/AddCommand.java
@@ -2,6 +2,7 @@ package com.laporeon.taskr.commands;
 
 import com.laporeon.taskr.enums.Color;
 import com.laporeon.taskr.enums.TaskPriority;
+import com.laporeon.taskr.enums.TaskStatus;
 import com.laporeon.taskr.repositories.TaskRepository;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -12,22 +13,28 @@ import picocli.CommandLine.Command;
 )
 public class AddCommand implements Runnable {
 
-    private static final TaskRepository taskRepository = new TaskRepository();
+    private final TaskRepository taskRepository;
+
+    public AddCommand(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     @CommandLine.Parameters(index = "0", description = "Task title")
     private String title;
 
-    @CommandLine.Option(names = {"-d", "--description"},
-            description = "Optional task description")
-    private String description;
+    @CommandLine.Option(names = {"-s", "--status"},
+            description = "Optional task status (todo, in-progress,done). Default: todo",
+            defaultValue = "todo")
+    private String status;
 
     @CommandLine.Option(names = {"-p", "--priority"},
-            description = "Optional task priority (low, medium, high). Default: low")
+            description = "Optional task priority (low, medium, high). Default: low",
+            defaultValue = "low")
     private String priority;
 
     @Override
     public void run() {
-        taskRepository.createTask(title, description, TaskPriority.fromString(priority.toLowerCase()));
+        taskRepository.createTask(title, TaskStatus.fromString(status), TaskPriority.fromString(priority));
         System.out.printf("%s✔ Task successfully created!%s", Color.GREEN, Color.RESET);
     }
 }
