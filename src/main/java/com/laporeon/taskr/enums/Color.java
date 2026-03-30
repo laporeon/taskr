63844dc85cf80e347c781ac8b69f5ff3d4771c77
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-30 15:29:35 -0300

    feat(enums): add new colors to Color enum and create apply method

--- src/main/java/com/laporeon/taskr/enums/Color.java
+++ src/main/java/com/laporeon/taskr/enums/Color.java
@@ -3,12 +3,23 @@ package com.laporeon.taskr.enums;
 public enum Color {
     RED("\u001B[31m"),
     GREEN("\u001B[32m"),
-    RESET("\u001B[0m");
+    RESET("\u001B[0m"),
+    BOLD_UNDERLINE("\u001B[1;4m"),
+    YELLOW("\u001B[33m"),
+    ORANGE("\u001B[38;5;166m"),
+    BLUE("\u001B[34m"),
+    GRAY("\u001B[90m"),
+    STRIKETHROUGH("\u001B[9m"),
+    GRAY_STRIKETHROUGH("\u001B[90m\u001B[9m");
 
     private final String ansiCode;
 
     Color(String ansiCode) {
         this.ansiCode = ansiCode;
+    }
+
+    public String apply(String symbol) {
+        return ansiCode + symbol + Color.RESET.ansiCode;
     }
 
     @Override
