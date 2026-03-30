43cce93a48b3329c4be20cc24a603670f6eb9b2e
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-30 15:30:42 -0300

    refactor(enums): improve contract for Task enums and create method to get colored symbol

--- src/main/java/com/laporeon/taskr/enums/TaskPriority.java
+++ src/main/java/com/laporeon/taskr/enums/TaskPriority.java
@@ -7,25 +7,29 @@ import java.util.Arrays;
 @Getter
 public enum TaskPriority {
 
-    LOW("low", "●○○", "\u001B[32m"),
-    MEDIUM("medium", "●●○", "\u001B[33m"),
-    HIGH("high", "●●●", "\u001B[31m");
+    LOW("low", "●○○", Color.GREEN),
+    MEDIUM("medium", "●●○", Color.YELLOW),
+    HIGH("high", "●●●", Color.RED);
 
     private final String value;
     private final String symbol;
-    private final String color;
+    private final Color color;
 
-    TaskPriority(String value, String symbol, String color) {
+    TaskPriority(String value, String symbol, Color color) {
         this.value = value;
         this.symbol = symbol;
         this.color = color;
+    }
+
+    public String getColoredSymbol() {
+        return color.apply(symbol);
     }
 
     private static final String INVALID_PRIORITY_VALUE_ERROR = "Invalid priority '%s'. Valid options are: low, medium or high";
 
     public static TaskPriority fromString(String priority) {
         return Arrays.stream(TaskPriority.values())
-                .filter(task -> task.value.equalsIgnoreCase(priority))
+                .filter(p -> p.value.equalsIgnoreCase(priority))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_PRIORITY_VALUE_ERROR.formatted(priority)));
     }
--- src/main/java/com/laporeon/taskr/enums/TaskStatus.java
+++ src/main/java/com/laporeon/taskr/enums/TaskStatus.java
@@ -7,25 +7,29 @@ import java.util.Arrays;
 @Getter
 public enum TaskStatus {
 
-    TODO("todo", "[ ]", "\u001B[38;5;208m"),
-    IN_PROGRESS("in-progress", "[~]", "\u001B[34m"),
-    DONE("done","[✓]", "\u001B[32m");         // Green
+    TODO("todo", "[ ]", Color.ORANGE),
+    IN_PROGRESS("in-progress", "[~]", Color.BLUE),
+    DONE("done","[✓]", Color.GREEN);         // Green
 
     private final String value;
     private final String symbol;
-    private final String color;
+    private final Color color;
 
-    TaskStatus(String value, String symbol, String color) {
+    TaskStatus(String value, String symbol, Color color) {
         this.value = value;
         this.symbol = symbol;
         this.color = color;
+    }
+
+    public String getColoredSymbol() {
+        return color.apply(symbol);
     }
 
     private static final String INVALID_STATUS_VALUE_ERROR = "Invalid status '%s'. Valid options are: todo, in-progress or done";
 
     public static TaskStatus fromString(String status) {
         return Arrays.stream(TaskStatus.values())
-                .filter(task -> task.value.equalsIgnoreCase(status))
+                .filter(s -> s.value.equalsIgnoreCase(status))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_STATUS_VALUE_ERROR.formatted(status)));
     }
