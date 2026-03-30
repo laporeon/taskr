c18f6dea899b24636c0e5eeb5fbb9baede71eb18
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-30 15:32:09 -0300

    feat(cli): create listTasks and getLastIndex methods on repository layer

--- src/main/java/com/laporeon/taskr/repositories/TaskRepository.java
+++ src/main/java/com/laporeon/taskr/repositories/TaskRepository.java
@@ -2,18 +2,32 @@ package com.laporeon.taskr.repositories;
 
 import com.laporeon.taskr.entities.Task;
 import com.laporeon.taskr.enums.TaskPriority;
+import com.laporeon.taskr.enums.TaskStatus;
 import com.laporeon.taskr.helpers.FileStorageHandler;
+
+import java.util.List;
 
 public class TaskRepository {
 
-    public void createTask(String title, String description, TaskPriority priority) {
+    public void createTask(String title, TaskStatus status, TaskPriority priority) {
+        int lastIndex = getLastIndex();
+
         Task task = Task.builder()
+                .id(lastIndex + 1)
                 .title(title)
-                .description(description)
+                .status(status)
                 .priority(priority)
                 .build();
 
-        FileStorageHandler.saveTaskToFile(task);
+        FileStorageHandler.saveToFile(task);
+    }
+
+    public List<Task> listTasks() {
+        return FileStorageHandler.readFile();
+    }
+
+    private Integer getLastIndex() {
+        return listTasks().size();
     }
 
 }
