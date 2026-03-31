2398a8bc146565a62f7361a336cc091d1cd49912
laporeon <175569928+laporeon@users.noreply.github.com>
2026-03-31 19:25:24 -0300

    feat(cli): create delete task method on repository layer

--- src/main/java/com/laporeon/taskr/repositories/TaskRepository.java
+++ src/main/java/com/laporeon/taskr/repositories/TaskRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public class TaskRepository {
 
+    private static final String INVALID_ID_MESSAGE = "Invalid or missing task ID: %d. Please try again...";
+
     public void createTask(String title, TaskStatus status, TaskPriority priority) {
         Task task = Task.builder()
                 .id(getNextId())
@@ -22,6 +24,18 @@ public class TaskRepository {
 
     public List<Task> listTasks() {
         return FileStorageHandler.readFile();
+    }
+
+    public void deleteTask(int id) {
+        List<Task> tasks = FileStorageHandler.readFile();
+
+        boolean isDeleted = tasks.removeIf(task -> task.getId() == id);
+
+        if (!isDeleted) {
+            throw new IllegalArgumentException(String.format(INVALID_ID_MESSAGE, id));
+        }
+
+        FileStorageHandler.overwriteFileContent(tasks);
     }
 
     private Integer getNextId() {
