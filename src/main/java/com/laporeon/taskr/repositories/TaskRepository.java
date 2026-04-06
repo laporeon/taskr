0cb64167ba28ba697e2260d461aa7cf0da024f23
laporeon <175569928+laporeon@users.noreply.github.com>
2026-04-06 18:17:19 -0300

    refactor(cli): create private method to retrieve task by index

--- src/main/java/com/laporeon/taskr/repositories/TaskRepository.java
+++ src/main/java/com/laporeon/taskr/repositories/TaskRepository.java
@@ -5,18 +5,22 @@ import com.laporeon.taskr.enums.TaskPriority;
 import com.laporeon.taskr.enums.TaskStatus;
 import com.laporeon.taskr.helpers.FileStorageHandler;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 
 public class TaskRepository {
 
-    private static final String INVALID_ID_MESSAGE = "Invalid or missing task ID: %d. Please try again...";
+    private static final String INVALID_INDEX_MESSAGE = "Invalid or missing task index: %d. Please try again...";
 
     public void createTask(String title, TaskStatus status, TaskPriority priority) {
         Task task = Task.builder()
-                .id(getNextId())
+                .id(UUID.randomUUID())
                 .title(title)
                 .status(status)
                 .priority(priority)
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
                 .build();
 
         FileStorageHandler.saveToFile(task);
@@ -26,33 +30,31 @@ public class TaskRepository {
         return FileStorageHandler.readFile();
     }
 
-    public void updateTask(int id, String title, TaskStatus status, TaskPriority priority) {
+    public void updateTask(int index, String title, TaskStatus status, TaskPriority priority) {
         List<Task> tasks = FileStorageHandler.readFile();
 
-        Task task = tasks.stream()
-                         .filter(t -> t.getId() == id)
-                         .findFirst()
-                         .orElseThrow(() -> new IllegalArgumentException(String.format(INVALID_ID_MESSAGE, id)));
-
+        Task task = getTaskByIndex(tasks, index);
         task.update(title, status, priority);
 
         FileStorageHandler.overwriteFileContent(tasks);
     }
 
-    public void deleteTask(int id) {
+    public void deleteTask(int index) {
         List<Task> tasks = FileStorageHandler.readFile();
 
-        boolean isDeleted = tasks.removeIf(task -> task.getId() == id);
-
-        if (!isDeleted) {
-            throw new IllegalArgumentException(String.format(INVALID_ID_MESSAGE, id));
-        }
+        Task task = getTaskByIndex(tasks, index);
+        tasks.remove(task);
 
         FileStorageHandler.overwriteFileContent(tasks);
     }
 
-    private Integer getNextId() {
-        return listTasks().size() + 1;
-    }
+    private Task getTaskByIndex(List<Task> tasks, int index) {
+        int size = tasks.size();
 
+        if (index < 1 || index > size) {
+            throw new IllegalArgumentException(String.format(INVALID_INDEX_MESSAGE, index));
+        }
+
+        return tasks.get(index - 1);
+    }
 }
