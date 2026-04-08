822e901f01daebf0c0bcae4b51c1f4e74a3293bb
laporeon <175569928+laporeon@users.noreply.github.com>
2026-04-08 14:32:45 -0300

    feat(helpers): create Serializer helper class to handle task format methods

---
+++ src/main/java/com/laporeon/taskr/helpers/TaskSerializer.java
@@ -0,0 +1,41 @@
+package com.laporeon.taskr.helpers;
+
+import com.laporeon.taskr.entities.Task;
+import com.laporeon.taskr.enums.Color;
+import com.laporeon.taskr.enums.TaskStatus;
+
+public class TaskSerializer {
+
+    public static String toCsvString(Task task) {
+        return String.format(
+                "%s;%s;%s;%s;%s;%s",
+                task.getId(),
+                task.getTitle(),
+                task.getStatus().getValue(),
+                task.getPriority().getValue(),
+                task.getCreatedAt(),
+                task.getUpdatedAt()
+        );
+    }
+
+    public static String toDisplayString(Task task, int index) {
+        String formattedIndex = index + ".";
+
+        String displayIndex = (task.getStatus() == TaskStatus.DONE)
+                ? Color.GRAY.apply(formattedIndex)
+                : formattedIndex;
+
+        String displayTitle = (task.getStatus() == TaskStatus.DONE)
+                ? Color.GRAY_STRIKETHROUGH.apply(task.getTitle())
+                : task.getTitle();
+
+        return String.format(
+                "%s %s %s %s",
+                displayIndex,
+                task.getStatus().getColoredSymbol(),
+                displayTitle,
+                task.getPriority().getColoredSymbol()
+        );
+    }
+
+}
