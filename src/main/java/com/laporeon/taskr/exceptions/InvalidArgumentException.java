6d1ff03d9e8270447e116a4fa6a4781117d7ce71
laporeon <175569928+laporeon@users.noreply.github.com>
2026-04-08 14:29:51 -0300

    feat(cli): create custom exceptions to improve error logging

---
+++ src/main/java/com/laporeon/taskr/exceptions/InvalidArgumentException.java
@@ -0,0 +1,9 @@
+package com.laporeon.taskr.exceptions;
+
+public class InvalidArgumentException extends RuntimeException {
+
+    public InvalidArgumentException(String message) {
+        super(message);
+    }
+
+}
