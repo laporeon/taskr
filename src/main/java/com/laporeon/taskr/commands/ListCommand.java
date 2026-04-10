67a1f5c83811d15c54757f0a1aa66d8349e945fb
laporeon <175569928+laporeon@users.noreply.github.com>
2026-04-10 18:28:21 -0300

    feat(cli): add filter by status option in list tasks command

--- src/main/java/com/laporeon/taskr/commands/ListCommand.java
+++ src/main/java/com/laporeon/taskr/commands/ListCommand.java
@@ -1,15 +1,17 @@
 package com.laporeon.taskr.commands;
 
 import com.laporeon.taskr.entities.Task;
+import com.laporeon.taskr.enums.TaskStatus;
 import com.laporeon.taskr.helpers.TaskboardRender;
 import com.laporeon.taskr.services.TaskService;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 import java.util.List;
 
 @Command(
         name = "list",
-        description = "List all tasks.",
+        description = "List all tasks or fetch tasks by status.",
         mixinStandardHelpOptions = true
 )
 public class ListCommand implements Runnable {
@@ -20,9 +22,16 @@ public class ListCommand implements Runnable {
         this.taskService = taskService;
     }
 
+    @CommandLine.Option(
+            names = {"-s", "--status"},
+            paramLabel = "STATE",
+            description = "Optional task status (todo, in-progress, done)"
+    )
+    private String status;
+
     @Override
     public void run() {
-        List<Task> tasks = taskService.listTasks();
+        List<Task> tasks = taskService.listTasks(TaskStatus.fromString(status));
         System.out.println(TaskboardRender.renderTaskboard(tasks));
     }
 }
