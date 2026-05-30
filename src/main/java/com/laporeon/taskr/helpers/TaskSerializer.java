53abe5362d5fc6dd61ba398dc3f06e18aaeb48a5
laporeon <175569928+laporeon@users.noreply.github.com>
2026-05-30 15:42:34 -0300

    feat: create toEntity and fromFields methods to improve serialization

--- src/main/java/com/laporeon/taskr/helpers/TaskSerializer.java
+++ src/main/java/com/laporeon/taskr/helpers/TaskSerializer.java
@@ -2,9 +2,35 @@ package com.laporeon.taskr.helpers;
 
 import com.laporeon.taskr.entities.Task;
 import com.laporeon.taskr.enums.Color;
+import com.laporeon.taskr.enums.TaskPriority;
 import com.laporeon.taskr.enums.TaskStatus;
 
+import java.time.Instant;
+import java.util.UUID;
+
 public class TaskSerializer {
+
+    public static Task fromFields(String[] attributes) {
+        Task task = new Task();
+        task.setId(UUID.fromString(attributes[0]));
+        task.setTitle(attributes[1]);
+        task.setStatus(TaskStatus.fromString(attributes[2]));
+        task.setPriority(TaskPriority.fromString(attributes[3]));
+        task.setCreatedAt(Instant.parse(attributes[4]));
+        task.setUpdatedAt(Instant.parse(attributes[5]));
+        return task;
+    }
+
+    public static Task toEntity(String title, TaskStatus status, TaskPriority priority) {
+        Task task = new Task();
+        task.setId(UUID.randomUUID());
+        task.setTitle(title);
+        task.setStatus(status);
+        task.setPriority(priority);
+        task.setCreatedAt(Instant.now());
+        task.setUpdatedAt(Instant.now());
+        return task;
+    }
 
     public static String toCsvString(Task task) {
         return String.format(
